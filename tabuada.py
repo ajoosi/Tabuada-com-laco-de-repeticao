3f0bc45e6d07107bf46d65be9f6f28cf63831f81
Josiane Andrade <josideandrade1@hotmail.com>
2018-07-04 18:55:12 -0300

    novo comm

---
+++ tabuada.py
@@ -0,0 +1,13 @@
+#while True --Looping infinito
+# For c in range --Laço de repetição
+# if n < 0: --- Se o numero digitado for menor que zero (ou seja,NEGATIVO) pare o programa.
+
+while True:
+    n = int(input('Quer ver a tabuada de qual valor?'))
+    if n < 0:
+        break
+    print('--'* 30)
+    for c in range(1,11):
+        print(f'{n} x {c}  = {n*c}')
+    print('--'*30)
+print('PROGRAMA TABUADA ENCERRADO.')
